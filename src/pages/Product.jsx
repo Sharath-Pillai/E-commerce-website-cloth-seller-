68d05aac21f48752a7e5a5006fa5af70a3da498b
sharath pillai <spillai.developer@gmail.com>
2026-02-26 22:34:16 +0530

    Product page modified

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import ShopContext from "../context/ShopContext";
 import { useParams } from "react-router-dom";
 import RelatedProducts from "../components/RelatedProducts";
+import { assets } from "../assets/frontend_assets/assets";
 
 const Product = () => {
   const { productId } = useParams();
@@ -106,7 +107,10 @@ const Product = () => {
         </div>
       </div>
       {/* display related products */}
-      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
+      <RelatedProducts
+        category={productData.category}
+        subCategory={productData.subCategory}
+      />
     </div>
   ) : (
     <div className="opacity-0">Loading...</div>
