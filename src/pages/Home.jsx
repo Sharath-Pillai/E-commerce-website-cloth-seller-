229507e970bae2e7ef3801a4a01ccdbb5744d102
sharath pillai <spillai.developer@gmail.com>
2026-02-26 13:32:19 +0530

    Home page fully completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import LatestCollection from '../components/latestCollection'
+import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/ourPolicy'
+import NewsletterBox from '../components/NewsletterBox'
+import Footer from '../components/Footer'
 const Home = () => {
   return (
     <div>
       <Hero />
+      <LatestCollection/>
+      <BestSeller/>
+      <OurPolicy/>
+      <NewsletterBox/>
+      <Footer/>
     </div>
   )
 }
