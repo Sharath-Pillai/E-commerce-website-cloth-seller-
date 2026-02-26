5d642d6af5e12b8f1c2457551bbe67a5c509c64e
sharath pillai <spillai.developer@gmail.com>
2026-02-26 13:30:30 +0530

    BestSeller and footer components created and fully completed for home page.Product list also added in the bestseller and latestcollection areas

---
+++ src/components/BestSeller.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react'
+import ShopContext from '../context/ShopContext'
+import Title from './Title'
+import ProductItem from './ProductItem'
+
+const BestSeller = () => {
+
+const {products}=useContext(ShopContext)
+const bestSeller=products.filter((item)=>item.bestSeller===true).slice(0,5)
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"Best"} text2={"Sellers"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero odio
+          recusandae laboriosam eligendi nostrum dolor omnis doloremque eveniet
+          cum . Officiis adipisci, quidem itaque blanditiis cumque fuga culpa
+          aperiam, amet veritatis minus, voluptas ratione incidunt iure a!
+        </p>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+          {bestSeller.map((item,index) => (
+            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BestSeller
