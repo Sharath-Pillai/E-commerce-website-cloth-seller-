ee5cbdd6bd3265cce20a59a7854e9237eb9aeef5
sharath pillai <spillai.developer@gmail.com>
2026-02-26 17:39:33 +0530

    App.jsx modified

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ import Orders from "./pages/Orders";
 import PlaceOrder from "./pages/PlaceOrder";
 import Product from "./pages/Product";
 import NavBar from "./components/NavBar.jsx";
+import SearchBar from "./components/searchBar.jsx";
 const App = () => {
   return (
     <div className="px-2 sm:px-4 md:px-6 lg:px-8">
       <NavBar />
+      <SearchBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LogIn />} />
