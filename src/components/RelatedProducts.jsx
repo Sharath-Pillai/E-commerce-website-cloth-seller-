bbe5f9bf5a6d75dec980899095ca62a2af69dd70
sharath pillai <spillai.developer@gmail.com>
2026-02-27 13:34:44 +0530

    RelatedProduct component created,completed,product but image not visible. This component used in product page

--- src/components/RelatedProducts.jsx
+++ src/components/RelatedProducts.jsx
@@ -1,30 +1,32 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import ShopContext from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
 
-const RelatedProducts = ({ category, subCategory }) => {
-  const { produts } = useContext(ShopContext);
-  const [related, setRelated] = useState([]);
-  useEffect(() => {
-    if (produts.length > 0) {
-      let productcopy = produts.slice();
-      productcopy = productcopy.filter((item) => category === item.category);
-      productcopy = productcopy.filter(
-        (item) => subCategory === item.subCategory,
-      );
-      setRelated(productcopy.slice(0, 5));
-    }
-  }, [produts]);
+const RelatedProducts = ({ category, subCategory, currentId }) => {
+  const { products, currency } = useContext(ShopContext);
+
+  const relatedProductData = [...products]
+    .filter(
+      (item) =>
+        category === item.category &&
+        subCategory === item.subCategory &&
+        currentId !== item._id,
+    )
+    .slice(0, 5);
+
+  if (!relatedProductData.length) return null;
+
   return (
     <div className="my-24">
       <div className="text-center text-3xl py-2">
         <Title text1={"Related"} text2={"Products"} />
       </div>
       <div className="flex justify-center">
-        <div className=" grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-          {related.map((item, index) => (
+        <div className=" grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 mt-8">
+          {relatedProductData.map((item) => (
             <ProductItem
-              key={index}
-              id={item._id}
+              key={item._id}
               name={item.name}
               image={item.image[0]}
               price={item.price}
