5afbc8132ad6ba189327e3ed861a97b9b972e82d
sharath pillai <spillai.developer@gmail.com>
2026-02-28 10:14:13 +0530

    latestcollection component modified

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -7,7 +7,6 @@ const LatestCollection = () => {
   const { products } = useContext(ShopContext);
   const latestProducts = products.slice(0, 10);
 
-  console.log(products);
   return (
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
