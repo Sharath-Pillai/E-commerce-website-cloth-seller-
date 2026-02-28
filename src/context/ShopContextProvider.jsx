447ca4de3df8b98bd358b2ec9b0a911fe262cdc9
sharath pillai <spillai.developer@gmail.com>
2026-02-28 10:21:28 +0530

    getCartCount and addToCart functions implemented

--- src/context/ShopContextProvider.jsx
+++ src/context/ShopContextProvider.jsx
@@ -1,12 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
-import  ShopContext  from "./ShopContext";
+import ShopContext from "./ShopContext";
+import { toast } from "react-toastify";
 
-const ShopContextProvider = ({children}) => {
+const ShopContextProvider = ({ children }) => {
   const currency = "$";
   const delivery_fee = 10;
-const[search,setSearch]=useState("")
-const[showSearch,setShowSearch]=useState(false)
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setcartItems] = useState({});
+
+  const addToCart = async (itemId, size) => {
+if(!size){
+  toast.error("Please select size")
+  return
+}
+
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    }else{
+      cartData[itemId]={}
+      cartData[itemId][size]=1
+    }
+    setcartItems(cartData)
+  };
+
+const getCartCount=()=>{
+  let totalCount=0
+  for(const items in cartItems){
+    for(const item in cartItems[items]){
+      try {
+        if(cartItems[items][item]>0){
+          totalCount+=cartItems[items][item]
+        }
+      } catch (error) {
+        console.log("error in size",error)
+      }
+    }
+  }
+  return totalCount
+}
+
+
+
+useEffect(()=>{
+console.log(cartItems)
+},[cartItems])
 
   const data = {
     products,
@@ -15,12 +59,10 @@ const[showSearch,setShowSearch]=useState(false)
     search,
     setSearch,
     showSearch,
-    setShowSearch
-
+    setShowSearch,
+    cartItems,addToCart,getCartCount
   };
-  return (
-    <ShopContext.Provider value={data}>{children}</ShopContext.Provider>
-  );
+  return <ShopContext.Provider value={data}>{children}</ShopContext.Provider>;
 };
 
 export default ShopContextProvider;
