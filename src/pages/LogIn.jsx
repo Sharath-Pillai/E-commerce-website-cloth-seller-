ce8ca38f2c76a247a727bdc4ff9521c9ad1a3488
sharath pillai <spillai.developer@gmail.com>
2026-03-02 19:38:33 +0530

    LogIn page created and fully completed, signUp option also implemented

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LogIn = () => {
+  const[currentState,setCurrentState]=useState("Login")
   return (
-    <div>Login</div>
+    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-500'>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p className='prata-regular text-3xl'>{currentState}</p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-800'/>
+      </div>
+      {currentState ==="Login"?"":<input type='text' className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required/>}
+      <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' />
+      <input type="password" className="w-full px-3 py-2 border border-gray-800" placeholder='Password' />
+      <div className='w-full flex justify-between text-sm -mt-2'>
+        <p className='cursor-pointer'>Forgot Your password?</p>
+        {currentState==="Login"?<p onClick={()=>setCurrentState("Sign Up")} className='cursor-pointer'>Create account</p>:<p onClick={()=>setCurrentState("Login")} className='cursor-pointer'>Login Here</p>}
+      </div>
+      
+      
+      
+      }
+
+    </form>
   )
 }
 
