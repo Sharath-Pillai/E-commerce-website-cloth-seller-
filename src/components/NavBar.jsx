c955f7366547faf34e60abe0e36ceadc6d1b83d2
sharath pillai <spillai.developer@gmail.com>
2026-03-02 19:39:47 +0530

    navBar component modified and profile icon link to login path

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -38,11 +38,14 @@ const{setShowSearch,getCartCount}=useContext(ShopContext)
           onClick={()=>setShowSearch(true)}
         />
         <div className="group relative">
+          <Link to="/login">
           <img
+          
             src={assets.profile_icon}
             alt="profile-icon"
             className="w-5 cursor-pointer"
           />
+          </Link>
           <div className="dropdown-menu group-hover:block  hidden right-0 pt-4 absolute">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profile</p>
