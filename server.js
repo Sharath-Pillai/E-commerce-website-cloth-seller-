5fb77dc8e6c2836bdcef951b94e56e36d2912c73
sharath pillai <spillai.developer@gmail.com>
2026-03-03 16:46:01 +0530

    server.js modified added connectDB and connectCloudinary fn

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
+import connectCoudinary from "./config/cloudinary.js";
 
 //App config
 const app = express();
 const port = process.env.PORT || 5000;
 connectDB()
-
+connectCoudinary()
 
 
 
