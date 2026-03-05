960007b5a4dde2753e87df89a4fa59a9bc68aa29
sharath pillai <spillai.developer@gmail.com>
2026-03-05 22:42:53 +0530

    App.jsx modified

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,31 +1,34 @@
 import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import SideBar from "./components/sideBar";
-import{Routes,Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Add from "./Pages/Add";
 import Login from "./components/Login";
 
-export const backendUrl= import.meta.env.VITE_BACKEND_URL
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
-  const[token,setToken]=useState('')
+  const [token, setToken] = useState("");
   return (
     <div className="bg-gray-50 min-h-screen">
-      {token ===""?<Login/>:<>
-        <NavBar />
-        <hr />
-        <div className="flex w-full">
-          <SideBar/>
-          <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
-            <Routes>
-              <Route path="/add" element={<Add/>}/>
-              <Route path="/list" element={<List/>}/>
-              <Route path="/orders" element={<Orders/>}/>
-            </Routes>
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <NavBar />
+          <hr />
+          <div className="flex w-full">
+            <SideBar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </>}
-      
+        </>
+      )}
     </div>
   );
 };
