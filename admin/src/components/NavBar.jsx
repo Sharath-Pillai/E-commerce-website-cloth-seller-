76c1e203ae683631d49f9bb602b3b78ff132976b
sharath pillai <spillai.developer@gmail.com>
2026-03-06 17:44:26 +0530

    setToken destructured and used inside button click to make token as empty string

--- admin/src/components/NavBar.jsx
+++ admin/src/components/NavBar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { assets } from "../../src/assets/admin_assets/assets.js";
-const NavBar = () => {
+const NavBar = ({setToken}) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between border-0 border-gray-400">
       <img className="w-[max(10%,80px)]" src={assets.logo} alt=" adminlogo" />
-      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">
+      <button onClick={()=>setToken("")} className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">
         LogOut
       </button>
     </div>
