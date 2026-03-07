e59f04de8098c8fc8d49a72b00cb2374ed1113be
sharath pillai <spillai.developer@gmail.com>
2026-03-07 13:25:07 +0530

    Login page desing and functionality implemented. used axios to collect data from frontend and it paasing to backend and mongodb during registration and login time. this time token will get and it saved into localstorage and navigate to home page

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,26 +1,108 @@
-import React, { useState } from 'react'
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import ShopContext from "../context/ShopContext";
 
 const LogIn = () => {
-  const[currentState,setCurrentState]=useState("Login")
-  const onSubmitHandler=(e)=>{
-    e.preventDefault()
-  }
-  return (
-    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-500'>
-      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
-        <p className='prata-regular text-3xl'>{currentState}</p>
-        <hr className='border-none h-[1.5px] w-8 bg-gray-800'/>
-      </div>
-      {currentState ==="Login"?"":<input type='text' className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required/>}
-      <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' />
-      <input type="password" className="w-full px-3 py-2 border border-gray-800" placeholder='Password' />
-      <div className='w-full flex justify-between text-sm -mt-2'>
-        <p className='cursor-pointer'>Forgot Your password?</p>
-        {currentState==="Login"?<p onClick={()=>setCurrentState("Sign Up")} className='cursor-pointer'>Create account</p>:<p onClick={()=>setCurrentState("Login")} className='cursor-pointer'>Login Here</p>}
-      </div>
-      
-    </form>
-  )
-}
+  const { token, setToken, navigate, backendUrl } = useContext(ShopContext);
+  const [currentState, setCurrentState] = useState("Sign Up");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      if (currentState === "Sign Up") {
+        const response = await axios.post(backendUrl + "/api/user/register", {
+          name,
+          email,
+          password,
+        });
+        if (response.data.success) {
+          setToken(response.data.token);
+          localStorage.setItem("token", token);
+        } else {
+          toast.error(response.data.message);
+        }
+      } else {
+        const response = await axios.post(backendUrl + "/api/user/login", {
+          email,
+          password,
+        });
+        if (response.data.success) {
+          setToken(response.data.token);
+          localStorage.setItem("token", token);
+        } else {
+          toast.error(response.data.message);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
 
-export default LogIn
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  }, [token]);
+  return (
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-500"
+    >
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+      {currentState === "Login" ? (
+        ""
+      ) : (
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          className="w-full px-3 py-2 border border-gray-800"
+          placeholder="Name"
+          required
+        />
+      )}
+      <input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        type="email"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Email"
+      />
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Password"
+      />
+      <div className="w-full flex justify-between text-sm -mt-2">
+        <p className="cursor-pointer">Forgot Your password?</p>
+        {currentState === "Login" ? (
+          <p
+            onClick={() => setCurrentState("Sign Up")}
+            className="cursor-pointer"
+          >
+            Create account
+          </p>
+        ) : (
+          <p
+            onClick={() => setCurrentState("Login")}
+            className="cursor-pointer"
+          >
+            Login Here
+          </p>
+        )}
+      </div>
+      <button className="border px-4 py-1 rounded bg-black text-white" type="submit">{currentState === "Login" ? "Login":"Sign Up"}</button>
+    </form>
+  );
+};
+
+export default LogIn;
