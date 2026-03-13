17fb01a7f207106b3b646b352932fcad03a4dce8
sharath pillai <spillai.developer@gmail.com>
2026-03-13 14:31:43 +0530

    order path modified

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -34,7 +34,7 @@ localStorage.setItem("token",token)
               <Routes>
                 <Route path="/add" element={<Add  token={token}/>} />
                 <Route path="/list" element={<List token={token}/>} />
-                <Route path="/orders" element={<Orders token={token}/>} />
+                <Route path="/order" element={<Orders token={token}/>} />
               </Routes>
             </div>
           </div>
