6529975d35c96de36f338a0b519203a04d998cfc
sharath pillai <spillai.developer@gmail.com>
2026-03-13 14:09:56 +0530

    error in localstorage setitem resolved

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -20,7 +20,7 @@ const LogIn = () => {
         });
         if (response.data.success) {
           setToken(response.data.token);
-          localStorage.setItem("token", token);
+          localStorage.setItem("token", response.data.token);
         } else {
           toast.error(response.data.message);
         }
@@ -31,7 +31,7 @@ const LogIn = () => {
         });
         if (response.data.success) {
           setToken(response.data.token);
-          localStorage.setItem("token", token);
+          localStorage.setItem("token", response.data.token);
         } else {
           toast.error(response.data.message);
         }
